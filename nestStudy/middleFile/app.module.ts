88d5f78ca5cb0cc273b8e38da1940d7d336a8c48
YXR <913457727@qq.com>
2024-07-16 16:09:14 +0800

    feat: middleware 拦截器

---
+++ nestStudy/middleFile/app.module.ts
@@ -0,0 +1,15 @@
+/* 应用程序的根模块 */
+import { Module } from '@nestjs/common';
+// controller 是路由
+import { AppController } from './app.controller';
+// service 是业务
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [UserModule],
+  controllers: [AppController],
+  // https://docs.nestjs.cn/9/providers?id=providers
+  providers: [AppService],
+})
+export class AppModule {}
