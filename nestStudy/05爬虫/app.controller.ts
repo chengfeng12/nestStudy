4d50db91188b3669c0f6d485716f721ac55c7ba9
YXR <913457727@qq.com>
2024-08-07 10:59:26 +0800

    feat: 爬虫

---
+++ nestStudy/05爬虫/app.controller.ts
@@ -0,0 +1,13 @@
+/* 带有单个路由的基本控制器示例 */
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('user')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/hello')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
